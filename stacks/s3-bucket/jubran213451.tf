4d3098c22d11c1ef896141458aa88ce9e7e16dd8
Jubran <jubranNassar>
2025-09-05 09:49:31 -0500

    feat: create S3 bucket jubran213451

---
+++ stacks/s3-bucket/jubran213451.tf
@@ -0,0 +1,11 @@
+module jubran213451 {
+  source = "github.com/thoughtbot/terraform-s3-bucket?ref=v0.4.0"
+
+  name = "jubran213451"
+  tags = { 
+    
+    "key1" = "value1",
+    
+    "key2" = "value2",
+      }
+}
